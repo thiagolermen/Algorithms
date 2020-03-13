9ee6a61672f087d8c14d8ec35ce6076357a1af19
Thiago <thiago.lermen@gmail.com>
2020-03-13 20:55:54 -0300

    added insertion sort

---
+++ src/sorting/InsertionSort.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+void insertionSort(int C[], int n){
+    int i, key, j;
+    for(j=1;j<n;j++){
+        key = C[j];
+        // insert the key in the ordered sequence C[1..j-1]
+        i = j-1;
+        while((i>=0) && (C[i]>key)){
+            C[i+1] = C[i];
+            i = i-1;
+        }
+        C[i+1] = key;
+    }
+}
+
+void printArray(int C[], int n){
+    for(int i=0;i<n;i++)
+        cout << "[" << C[i] << "] ";
+    cout << endl;
+}
+
+
+int size(){
+    return 100;
+    //return 1000
+}
+
+int main(){
+    int C[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    cout << "CURRENT ARRAY: " << endl;
+    printArray(C, 10);
+    cout << "SORTED ARRAY: " << endl;
+    insertionSort(C, 10);
+    printArray(C, 10); 
+
+    return 0;
+}
