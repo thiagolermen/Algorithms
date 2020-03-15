1d863e13059fb107af7bf400cc4f0c91f4b16c27
Thiago <thiago.lermen@gmail.com>
2020-03-15 11:17:32 -0300

    added shell sort

---
+++ src/sorting/ShellSort.cpp
@@ -0,0 +1,47 @@
+/*
+    Shell Sort
+    Author: Thiago Sotoriva Lermen
+    Complexity: O(n²) or Θ(n*log n)
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void shellSort(int C[], int n){
+    int gap = 1;
+    do{
+        gap = 3*gap+1;
+    }while(gap < n);
+
+    do{
+        gap /= 3;
+        for(int i=gap;i<n;i++){
+            int value = C[i];
+            int j = i-gap;
+
+            while(j>=0 && value < C[j]){
+                C[j+gap] = C[j];
+                j-=gap;
+            }
+            C[j+gap] = value;
+        }
+    }while(gap > 1);
+}
+
+void printArray(int C[], int n){
+    for(int i=0;i<n;i++)
+        cout << "[" << C[i] << "] ";
+    cout << endl;
+}
+
+int main(){
+    int C[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    cout << "CURRENT ARRAY: " << endl;
+    printArray(C, 10);
+    cout << "SORTED ARRAY: " << endl;
+    shellSort(C, 10);
+    printArray(C, 10); 
+
+    return 0;
+}
